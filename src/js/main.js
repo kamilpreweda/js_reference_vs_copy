f663e8c0f154f346a09f0bef77f6af1635d85f0c
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-25 15:12:46 +0200

    added js objectives

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,28 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
-
-console.log(`Hello world!`);
-
+// start with strings, numbers and booleans
+// Let's say we have an array
+const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+// and we want to make a copy of it.
+// You might think we can just do something like this:
+// however what happens when we update that array?
+// now here is the problem!
+// oh no - we have edited the original array too!
+// Why? It's because that is an array reference, not an array copy. They both point to the same array!
+// So, how do we fix this? We take a copy instead!
+// one way
+// or create a new array and concat the old one in
+// or use the new ES6 Spread
+// now when we update it, the original one isn't changed
+// The same thing goes for objects, let's say we have a person object
+// with Objects
+const person = {
+  name: 'Wes Bos',
+  age: 80
+};
+    // and think we make a copy:
+    // how do we take a copy instead?
+    // We will hopefully soon see the object ...spread
+    // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
 
